31c648c275e929d90c9369008f738a3ec5b9d29b
TahsinProduction <TahsinProduction007@gmail.com>
2020-03-29 00:15:54 +0600

    sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -40,16 +40,8 @@ export class Sidebar extends Component {
             />
           </span>
         </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+         <span class="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <Scrollspy
